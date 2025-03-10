09c3397f974300ea3119fd9730019e2e8e2c7d70
Sconrad03 <tfcnavy03@gmail.com>
2025-03-10 10:37:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,35 @@
 provider "aws" {
+  version = "~> 4.0"
   region = "ap-south-1"
   access_key = "AKIAVFIWIUXH6Y3TR7F3"
   secret_key = "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"
 }
 
-resource "aws_s3_bucket" "env0bucket" {
-  bucket = "bucketdemo"
-  acl = "private"
+provider "random" {}
+
+resource "random_string" "random" {
+  length = "16"
+  special = "false"
+  min_lower = "16"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "env0-s3-${random_string.random.result}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
