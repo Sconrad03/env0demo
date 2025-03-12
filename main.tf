44c85a1bef55148d90d3734c0470ec6b515155e8
Sconrad03 <tfcnavy03@gmail.com>
2025-03-12 08:11:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
   region = "us-west-1"
-  access_key = "AKIAVFIWIUXH6Y3TR7F3"
-  secret_key = "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"
 }
 
 resource "aws_s3_bucket" "env0bucket" {
