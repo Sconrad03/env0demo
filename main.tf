397e82fe805cb819a15130e833c0a29e37466f1e
Sconrad03 <tfcnavy03@gmail.com>
2025-03-12 14:18:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_s3_bucket" "kosta_bucket" {
-  bucket = "kosta-bucket-from-terraform"
+resource "aws_s3_bucket" "env0bucket" {
+  bucket = "bucketdemo"
 }
