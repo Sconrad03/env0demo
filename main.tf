308da21f9cbc3b5163ee657289842e4b5b32f8f5
Sconrad03 <tfcnavy03@gmail.com>
2025-03-12 14:48:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-west-1"
 
  assume_role {
-    role_arn = "arn:aws:iam::123456789012:role/github-actions-role"
+    role_arn = "arn:aws:iam::354918376911:user/demos3"
   }
 }
 
